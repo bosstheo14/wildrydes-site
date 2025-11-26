fafd1696a890f0c48e514356978e8fe69a542417
bosstheo14 <143744314+bosstheo14@users.noreply.github.com>
2025-11-26 09:07:48 -0600

    Update Cognito configuration in config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_yjeJBmjEM', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2i3p5gl0t2detk1kl5mpb4d4mo', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
